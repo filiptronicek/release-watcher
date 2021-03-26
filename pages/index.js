87a7fcd83fe0266708f63625187ac6c87eee25f5
Filip Troníček <filip.tronicek@seznam.cz>
2021-03-26 18:29:02 +0000

    Clean up

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,42 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime);
 
 export default function Home() {
+
+  const dateF = (timeStamp) => {
+    const dateToRender = dayjs().to(dayjs(timeStamp));
+    return (
+      <span>{dateToRender}</span>
+    )
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Release watcher</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Release watcher
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          The place to get deploy information about GitHub and GitLab{' '}
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>GitHub.com &rarr;</h3>
+            <p>Deployed from fkwfinfAh {dateF(1616783267318)}</p>
           </a>
 
           <a
@@ -51,13 +52,13 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
+      Made by&nbsp;
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://trnck.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Filip
         </a>
       </footer>
     </div>
